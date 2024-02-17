3897da3797886c24b7ac2d653f7f7359cb7c30a1
Vlad B <bondar.vladislav1121@vu.cdu.edu.ua>
2024-02-17 23:59:18 +0200

    Scripts by sample

--- Assets/ObjectTracking.cs
+++ Assets/ObjectTracking.cs
@@ -6,13 +6,11 @@ public class ObjectTracking : MonoBehaviour
 {
     public Transform playerTransform;
     public Vector3 offset;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = playerTransform.position + offset;
